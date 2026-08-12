2235a9e244b61969d76972619e65688a212ba37a
WidgetUse <shakenfs@gmail.com>
2026-08-12 16:06:50 +0300

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,22 +1,36 @@
+
 const fm = FileManager.iCloud();
 const path = fm.joinPath(fm.documentsDirectory(), "widget_url.txt");
-let imgUrl = "https://widgetuse.github.io/id1.png";
-if (fm.fileExists(path)) {
-  imgUrl = fm.readString(path).trim(); 
-}
 
-const req = new Request(imgUrl);
-const image = await req.loadImage();
+let imgUrl = "https://widgetuse.github.io/id1.png";
+
+if (fm.fileExists(path)) {
+  const savedUrl = fm.readString(path).trim();
+  if (savedUrl.length > 0) {
+    imgUrl = savedUrl;
+  }
+}
 
 const widget = new ListWidget();
 widget.backgroundColor = new Color("#0a0c10");
 
-const wImg = widget.addImage(image);
-wImg.centerAlign();
-
+try {
+  const req = new Request(imgUrl);
+  const image = await req.loadImage();
+  
+  const wImg = widget.addImage(image);
+  wImg.centerAlign();
+  wImg.imageSize = new Size(130, 130);
+} catch (e) {
+  const errorText = widget.addText("⚠️ Картинку не знайдено");
+  errorText.textColor = Color.red();
+  errorText.centerAlign();
+  errorText.font = Font.boldSystemFont(12);
+}
 if (config.runsInWidget) {
   Script.setWidget(widget);
 } else {
-  widget.presentSmall();
+  await widget.presentSmall(); 
 }
+
 Script.complete();
